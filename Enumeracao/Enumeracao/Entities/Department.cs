a9bd0c926f6255a15a3ad62c0ea3c022c64afb85
Vinicius Bistao <vinicius.martins23@fatec.sp.gov.br>
2021-07-14 16:58:52 -0300

    initial Commit

---
+++ Enumeracao/Enumeracao/Entities/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enumeracao.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ novoStringBuilder/novoStringBuilder/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace novoStringBuilder.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
